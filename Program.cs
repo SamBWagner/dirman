69ff696f601b42b24a54e235f5c35b4dcdc0f8c7
Sam Wagner <samuel.b.wagner@gmail.com>
2025-11-06 19:41:15 +1000

    Add Docker.NET

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Docker.DotNet;
+using Docker.DotNet.Models;
+
+Console.WriteLine("Starting dirman...");
+
+var dockerClient = new DockerClientConfiguration().CreateClient();
+
+try
+{
+    await dockerClient.Containers.ListContainersAsync(new ContainersListParameters());
+}
+catch (Exception)
+{
+    Console.WriteLine("Dirman failed: Docker didn't dockerz. Start it you n00b.");
+    return;
+}
+
+Console.WriteLine("Listing running containers");
+var containers = await dockerClient.Containers.ListContainersAsync(new ContainersListParameters());
+foreach (var container in containers)
+{
+    Console.WriteLine("ID: " + container.ID);
+    Console.WriteLine("State: "+ container.State);
+    Console.WriteLine("Image: " + container.Image);
+    Console.WriteLine("ImageID:" + container.ImageID);
+}
+
+Console.WriteLine("Listing images...");
+var images = await dockerClient.Images.ListImagesAsync(new ImagesListParameters());
+foreach (var image in images)
+{
+    Console.WriteLine("ID: " + image.ID);
+    Console.WriteLine("Size: " + image.Size);
+}
+
+Console.WriteLine("Stopping dirman...");
